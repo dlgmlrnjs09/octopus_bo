c12337955ad999bca330356c70888a6250de6117
hrin0820 <hyerin@weaverloft.com>
2023-06-12 16:30:57 +0900

    [퍼블리싱] 통계 필터 관련 마크업 전달

--- src/main/webapp/asset/js/basic_admin.js
+++ src/main/webapp/asset/js/basic_admin.js
@@ -98,6 +98,36 @@ if ($(".calendar").length > 0) {
         });
     });
 }
+
+/*========== Datepicker ==========*/
+$(function(){
+	if($(".datepicker-box").length > 0) {
+		$('.datepicker').datepicker({
+            closeText: "닫기",
+            prevText: "이전달",
+            nextText: "다음달",
+            currentText: "오늘",
+			dateFormat: 'yy-mm-dd',
+			inline: true,
+			showOtherMonths: true,
+			showMonthAfterYear: true,
+			monthNames: [ '1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월' ],
+			monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+			dayNames: ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"],
+            dayNamesShort: ["일", "월", "화", "수", "목", "금", "토"],
+            dayNamesMin: ["S", "M", "T", "W", "T", "F", "S"],
+            firstDay: 0,
+            isRTL: false,
+            yearSuffix: "년",
+			constrainInput: false,
+			changeMonth: true,
+			changeYear: true,
+			yearRange: "-10:+10",
+		});
+		$('.datepicker').not('.not').datepicker('setDate', 'today');
+	}
+});
+
 /*========== Event ==========*/
 $(function(){
     if ($(".event-swiper").length > 0) {
